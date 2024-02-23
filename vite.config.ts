2686b47a4df4f438c948c684990d318b2be4c2cd
Izzuddin Natsir <32354148+izznatsir@users.noreply.github.com>
2024-02-23 13:59:21 +0700

    chore: move websocket plugin as patch

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { vitePlugin as remix, cloudflareDevProxyVitePlugin as cloudflare } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import cloudflareWebsocket from "./plugins/websocket-proxy";
 
 export default defineConfig({
-	plugins: [cloudflare(), cloudflareWebsocket(), remix(), tsconfigPaths()],
+	plugins: [cloudflare(), remix(), tsconfigPaths()],
 });
